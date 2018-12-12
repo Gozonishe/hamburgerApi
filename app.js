3a36f4be677bef7abbb526c80e4080f0516f7ae9
Andrey Travin <andrey.gozon@mail.ru>
2018-12-12 22:40:05 +0300

    +get index.html

--- app.js
+++ app.js
@@ -55,4 +55,12 @@ app.use((error, req, res, next) => {
     });
 });
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 module.exports = app;
